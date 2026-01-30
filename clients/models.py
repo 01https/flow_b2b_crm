12c79771d17b2c83f453fa033d37214f40af1b63
Fxhat <make772@ukr.net>
2026-01-30 23:28:24 +0200

    add safedelete library and configurate & add custom endpoints in ClientViewSet

--- clients/models.py
+++ clients/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from safedelete.models import SafeDeleteModel, SOFT_DELETE
 
 
-class Client(models.Model):
+class Client(SafeDeleteModel):
     """Model of clients for Kanban, Clients(Table), Client Details"""
+
     class Status(models.TextChoices):
         NEW = "new", "New"
         IN_PROGRESS = "in_progress", "In Progress"
         DONE = "done", "Done"
+
+    _safedelete_policy = SOFT_DELETE
 
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -15,12 +19,11 @@ class Client(models.Model):
         choices=Status.choices,
         default=Status.NEW
         )
-    email = models.EmailField(blank=True, unique=True)
-    phone = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(blank=True)
+    phone = models.CharField(max_length=20)
     comment = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.first_name}, {self.last_name}"
--- clients/views.py
+++ clients/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 
 from .models import Client
 from .serializers import ClientSerializer
@@ -7,3 +10,30 @@ from .serializers import ClientSerializer
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+
+    @action(detail=False, methods=["get"])
+    def archive(self, request):
+        archived_clients = Client.deleted_objects.all()
+        serializer = self.get_serializer(archived_clients, many=True)
+        return Response(serializer.data)
+    
+    @action(detail=True, methods=["post"])
+    def restore(self, request, pk=None):
+        try:
+            client = Client.all_objects.get(pk=pk)
+            
+            if not client.deleted:  # перевірка (опціонально)
+                return Response(
+                    {"error": "Client not in archive"}, 
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            
+            client.undelete()
+            serializer = self.get_serializer(client)
+            return Response(serializer.data)
+        
+        except Client.DoesNotExist:
+            return Response(
+                {"error": "Client not found"}, 
+                status=status.HTTP_404_NOT_FOUND
+            )
