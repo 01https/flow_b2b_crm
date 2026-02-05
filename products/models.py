f46a55263699efc730567279bf1691a6dd74c640
Fxhat <make772@ukr.net>
2026-02-05 22:35:52 +0200

    create model for products & add in settings MEDIA conf

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from safedelete.models import SafeDeleteModel, SOFT_DELETE
 
-# Create your models here.
+
+class Product(SafeDeleteModel):
+    """Model for Products Card"""
+    
+    _safedelete_policy = SOFT_DELETE
+    
+    img = models.ImageField(upload_to="products/")
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(0)]
+        )
+    description = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.name
