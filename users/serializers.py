f60080b5560092e9d7d8833545011f618f9d999a
Fxhat <make772@ukr.net>
2026-02-09 18:05:58 +0200

    create full MVS for users app

---
+++ users/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from django.contrib.auth import get_user_model
+
+from validators.common import (
+    validate_email,
+    validate_name,
+    validate_password
+)
+
+
+User = get_user_model()
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(validators=[validate_email])
+    first_name = serializers.CharField(validators=[validate_name])
+    last_name = serializers.CharField(validators=[validate_name])
+    password = serializers.CharField(
+        write_only=True,
+        validators=[validate_password]
+        )
+    password_confirm = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = User
+        fields = ["email", "password", "password_confirm", "first_name", "last_name"]
+    
+    def validate(self, data):
+        if data["password"] != data["password_confirm"]:
+            raise ValidationError({
+                "password_confirm": {"type": "PASSWORD_MISMATCH"}
+                })
+        return data
+    
+    def create(self, validated_data):
+        validated_data.pop("password_confirm")
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "email", "first_name", "last_name"]
+        read_only_fields = ["email"]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from users.serializers import UserRegistrationSerializer, UserSerializer
+
+User = get_user_model()
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return User.objects.filter(id=self.request.user.id)
+    
+    @action(detail=False, methods=["post"], permission_classes=[AllowAny])
+    def register(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({
+                "id": user.id,
+                "email": user.email,
+                "message": "User create successfully"
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    @action(detail=False, methods=["get"])
+    def me(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
