df77f12a21d03cab889afd32f7707e0e9bfa7dfd
Fxhat <make772@ukr.net>
2026-02-15 04:12:00 +0200

    add validators for name, email, phone, comment fields

--- clients/serializers.py
+++ clients/serializers.py
@@ -2,8 +2,12 @@ from rest_framework import serializers
 
 from .models import Client
 from products.models import Product
-
-
+from validators.common import (
+    validate_name,
+    validate_email,
+    validate_phone,
+    validate_comment
+)
 class ClientReadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
@@ -25,6 +29,11 @@ class ProductShortSerializer(serializers.ModelSerializer):
 
 
 class ClientDetailSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(validators=[validate_name])
+    last_name = serializers.CharField(validators=[validate_name])
+    email = serializers.EmailField(validators=[validate_email])
+    phone = serializers.CharField(validators=[validate_phone])
+    comment = serializers.CharField(validators=[validate_comment])
     products = ProductShortSerializer(many=True, read_only=True)
     products_ids = serializers.PrimaryKeyRelatedField(
         many=True,
